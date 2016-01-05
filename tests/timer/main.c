50d7c5b2e9e3e9bc38a82bb6375aaa2eecc07744
LittleBuster <DenisovS21@gmail.com>
2016-01-06 01:02:28 +0600

    refract tests,rm tmp

--- tests/timer/main.c
+++ tests/timer/main.c
@@ -1,4 +1,4 @@
-#include <extc/timer.h>
+#include <extc/stimer.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -9,15 +9,15 @@ void timer_callback(void *data)
 
 int main(int argc, char const *argv[]) {
     int ret_val;
-    struct timer tmr;
+    struct stimer tmr;
     char txt[22];
 
     puts("\nStarting test");
     puts("-----------------------\n");
 
-    timer_init(&tmr);
+    stimer_init(&tmr);
     strcpy(txt, "my some text");
-    ret_val = timer_create(&tmr, 1, &timer_callback, (void *)txt);
+    ret_val = stimer_create(&tmr, 1, &timer_callback, (void *)txt);
     if (ret_val == -1)
         puts("[FAIL] Timer creation");
     return 0;
